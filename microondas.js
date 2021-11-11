49e1662e65842b5649445ffb544c16c86ada5406
edersonalves <65741959+edersonalves@users.noreply.github.com>
2021-11-11 17:57:34 -0300

    Add files via upload

---
+++ microondas.js
@@ -0,0 +1,43 @@
+let Pipoca = 10;
+let Macarrão = 8;
+let Carne = 15;
+let Feijão = 12;
+let Brigadeiro = 8;
+
+    function microondas (prato,tempo){ 
+        
+        switch (prato){
+        
+        case 'Pipoca':
+            prato = Pipoca
+            break
+        case 'Macarrão':
+            prato = Macarrão
+            break
+        case 'Carne':
+            prato = Carne
+            break
+        case 'Feijão':
+            prato = Feijão
+            break
+        case 'Brigadeiro':
+            prato = Brigadeiro
+            break
+        default:
+            console.log("Prato Inexistente!/PIpiPIII")
+            break}
+
+        if(tempo>=prato && tempo<prato*2){
+            console.log("Prato pronto, bom apetite!!!/PIpiPIII")
+        }else if(tempo<prato){
+            console.log("Tempo insuficiente/PIpiPIII")
+        }else if(tempo>=prato*2&&tempo<=prato*3){
+            console.log("O prato queimou/PIpiPIII")
+        }else if(tempo>=prato*3){
+            console.log("Kabummmmmmm/PIpiPIII")
+        }
+    }
+/*Entrar sempre com um valor (texto, número, etc), e não com uma variável, a menos que a variável receba um input antes*/
+
+    
+microondas('Brigadeiro',8)
